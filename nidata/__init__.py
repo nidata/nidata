a5b6ee2cce725fd1739bff5c5e38e84e01bb565a
Ben Cipollini <bcipolli@ucsd.edu>
2015-07-08 08:50:23 -0700

    Add gzip monkey-patch

---
+++ nidata/__init__.py
@@ -0,0 +1,6 @@
+import gzip
+
+
+# Monkey-patch gzip to have faster reads on large gzip files
+if hasattr(gzip.GzipFile, 'max_read_chunk'):
+    gzip.GzipFile.max_read_chunk = 100 * 1024 * 1024  # 100Mb
