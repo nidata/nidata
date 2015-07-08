6d37bdcffbac45a329daf0cc94729f1f937f7648
Ben Cipollini <bcipolli@ucsd.edu>
2015-07-08 08:50:03 -0700

    Tweak paths, delete file.

--- test.py
+++
@@ -1,3 +0,0 @@
-from nidata.fetchers.aws_fetcher import aws_fetcher
-aws_fetcher('.', (('my_b0_LR_RL.nii.gz', 'HCP/100307/unprocessed/3T/Diffusion/100307_3T_BIAS_BC.nii.gz', {}),), profile_name='hcp')
-
