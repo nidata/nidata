eb91898a6b15a802e0dd59e79f029ca89babaf27
Ben Cipollini <bcipolli@ucsd.edu>
2015-07-09 09:25:20 -0700

    Don't have force=True by default.

--- nidata/core/datasets/__init__.py
+++ nidata/core/datasets/__init__.py
@@ -1,5 +1,5 @@
 class Dataset(object):
     dependencies = []
     def __init__(self)
-    def fetch(self, n_subjects=1, force=True, check=False, verbosity=1):
+    def fetch(self, n_subjects=1, force=False, check=False, verbosity=1):
         raise NotImplementedError()
