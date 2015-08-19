21b530ccde6fc610a01f5c2c3434fc66574a898c
Ben Cipollini <bcipolli@ucsd.edu>
2015-08-18 21:44:32 -0700

    Move my_connectome to multimodal (has diffusion data)

--- nidata/multimodal/my_connectome_2015/example1.py
+++ nidata/multimodal/my_connectome_2015/example1.py
@@ -1,5 +1,5 @@
 # from [AN IMPORT PATH] import [A CLASS]
-from nidata.functional.my_connectome_2015.datasets import MyConnectome2015Dataset
+from nidata.multimodal.my_connectome_2015.datasets import MyConnectome2015Dataset
 
 # runs the member function 'fetch' from the class 'Laumann2015Dataset'
 dataset = MyConnectome2015Dataset().fetch(n_sessions=5)
