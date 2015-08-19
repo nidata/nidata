0c08eae53b06384bd393238784604f21cc876e92
Ben Cipollini <bcipolli@ucsd.edu>
2015-08-18 21:42:44 -0700

    Enable access to every data file.

--- nidata/functional/my_connectome_2015/datasets.py
+++ nidata/functional/my_connectome_2015/datasets.py
@@ -19,28 +19,51 @@ from ...core.datasets import HttpDataset
 from ...core.fetchers import readmd5_sum_file
 
 
-class Laumann2015Dataset(HttpDataset):
+class MyConnectome2015Dataset(HttpDataset):
 # class [A CLASS]([A SUPER CLASS]) 
 
-    def fetch(self, n_subjects=1, fetch_stimuli=False,
-              url=None, resume=True, force=False, verbose=1):
+    def fetch(self, n_sessions=None, get_retinotopy=True, get_pilot=True,
+              get_diffusion=True, get_resting_state=True,
+              resume=True, force=False, verbose=1):
     		# before the fetcher, construct URLS to download
 			# Openfmri dataset ID ds000031
     		
-    	files = [('ds031','https://s3.amazonaws.com/openfmri/tarballs/ds031_retinotopy.tgz', {'uncompress':False}),
-    				 ]
+        # First, construct the relevant urls
+        files = []
+        opts = {'uncompress': True}
+        base_url = 'https://s3.amazonaws.com/openfmri/tarballs/'
 
-    				 # ('ds109_raw.tgz','https://s3.amazonaws.com/openfmri/tarballs/ds031_retinotopy.tgz', {'uncompress':True}),
-    				 # ('ds109_raw.tgz','https://s3.amazonaws.com/openfmri/tarballs/ds031_retinotopy.tgz', {'uncompress':True}),
-    				 # ('ds109_raw.tgz','https://s3.amazonaws.com/openfmri/tarballs/ds031_retinotopy.tgz', {'uncompress':True}),
-    				 # ('ds109_raw.tgz','https://s3.amazonaws.com/openfmri/tarballs/ds031_retinotopy.tgz', {'uncompress':True}),
-    				 # ('ds109_raw.tgz','https://s3.amazonaws.com/openfmri/tarballs/ds031_retinotopy.tgz', {'uncompress':True}),
-    				 # ]	
-						
+        if n_sessions >= 0 or n_sessions is None:  # this file: 13-24
+            files += [('ds031_set01', base_url + 'ds031_set01.tgz', opts)]
+        if n_sessions >= 13 or n_sessions is None:  # this file: 25-36
+            files += [('ds031_set01', base_url + 'ds031_set01.tgz', opts)]
+        if n_sessions >= 25 or n_sessions is None:  # this file: 37-48
+            files += [('ds031_set01', base_url + 'ds031_set01.tgz', opts)]
+        if n_sessions >= 37 or n_sessions is None:  # this file: 49-60
+            files += [('ds031_set01', base_url + 'ds031_set01.tgz', opts)]
+        if n_sessions >= 49 or n_sessions is None:  # this file: 61-72
+            files += [('ds031_set01', base_url + 'ds031_set01.tgz', opts)]
+        if n_sessions >= 61 or n_sessions is None:  # this file: 73-84
+            files += [('ds031_set01', base_url + 'ds031_set01.tgz', opts)]
+        if n_sessions >= 73 or n_sessions is None:  # this file: 85-97
+            files += [('ds031_set01', base_url + 'ds031_set01.tgz', opts)]
+        if n_sessions >= 86 or n_sessions is None:  # this file: 98-104
+            files += [('ds031_set01', base_url + 'ds031_set01.tgz', opts)]
+        if get_pilot:
+            files += [('ds031_pilot_set', base_url + 'ds031_pilot_set.tgz', opts)]
+        if get_resting_state:
+            files += [('ds031_ses105', base_url + 'ds031_ses105.tgz', opts)]
+        if get_diffusion:
+            files += [('ds031_ses106', base_url + 'ds031_ses106.tgz', opts)]
+        if get_retinotopy:
+            files += [('ds031_retinotopy', base_url + 'ds031_retinotopy.tgz', opts)]
 
+        # Now, fetch the files.
         files = self.fetcher.fetch(files, resume=resume, force=force, verbose=verbose)
 
-            # return the data
+        # Group the data according to modality.
+
+        # return the data
         return Bunch(func=files[1], session_target=files[0], mask=files[2],
                          conditions_target=files[3])
 
