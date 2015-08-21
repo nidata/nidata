0f5ddee6494bb5fe68b0b7b27d9a81ca09716e04
shual86 <ofergro@post.bgu.ac.il>
2015-08-21 10:27:29 -0700

    updating fetching and example

--- nidata/functional/my_dataset/datasets.py
+++ nidata/functional/my_dataset/datasets.py
@@ -2,7 +2,7 @@
 """
 Utilities to download functional MRI datasets
 """
-# Author: Alexandre Abraham, Philippe Gervais
+# Author: Ofer Groweiss
 # License: simplified BSD
 
 import os
@@ -14,91 +14,35 @@ from ...core.fetchers import readmd5_sum_file
 
 
 class MyDatasets(HttpDataset):
-    """Download and load an example haxby dataset
-
-    Parameters
-    ----------
-    data_dir: string, optional
-        Path of the data directory. Used to force data storage in a specified
-        location. Default: None
-
-    Returns
-    -------
-    data: sklearn.datasets.base.Bunch
-        Dictionary-like object, interest attributes are:
-        'func': string.  Path to nifti file with bold data.
-        'session_target': string. Path to text file containing session and
-        target data.
-        'mask': string. Path to nifti mask file.
-        'session': string. Path to text file containing labels (can be used
-        for LeaveOneLabelOut cross validation for example).
-
-
-    OR
-
-
-    Download and loads complete haxby dataset
-
-    Parameters
-    ----------
-    data_dir: string, optional
-        Path of the data directory. Used to force data storage in a specified
-        location. Default: None
-
-    n_subjects: int, optional
-        Number of subjects, from 1 to 6.
-
-    fetch_stimuli: boolean, optional
-        Indicate if stimuli images must be downloaded. They will be presented
-        as a dictionnary of categories.
-
-    Returns
-    -------
-    data: sklearn.datasets.base.Bunch
-        Dictionary-like object, the interest attributes are :
-        'anat': string list. Paths to anatomic images.
-        'func': string list. Paths to nifti file with bold data.
-        'session_target': string list. Paths to text file containing
-        session and target data.
-        'mask_vt': string list. Paths to nifti ventral temporal mask file.
-        'mask_face': string list. Paths to nifti ventral temporal mask file.
-        'mask_house': string list. Paths to nifti ventral temporal mask file.
-        'mask_face_little': string list. Paths to nifti ventral temporal
-        mask file.
-        'mask_house_little': string list. Paths to nifti ventral temporal
-        mask file.
-
-    References
-    ----------
-    `Haxby, J., Gobbini, M., Furey, M., Ishai, A., Schouten, J.,
-    and Pietrini, P. (2001). Distributed and overlapping representations of
-    faces and objects in ventral temporal cortex. Science 293, 2425-2430.`
-
-    Notes
-    -----
-    PyMVPA provides a tutorial making use of this dataset:
-    http://www.pymvpa.org/tutorial.html
-
-    More information about its structure:
-    http://dev.pymvpa.org/datadb/haxby2001.html
-
-    See `additional information
-    <http://www.sciencemag.org/content/293/5539/2425>`
-
-    Run 8 in subject 5 does not contain any task labels.
-    The anatomical image for subject 6 is unavailable.
-    """
+  
 
     def fetch(self, n_subjects=1, url=None, resume=True, force=False, verbose=1):
 
-		# URL of the dataset. It is optional because a test uses it to test dataset
-		# downloading
-		if url is None:
-			url = 'http://openfmri.s3.amazonaws.com/tarballs/ds052_raw.tgz'
+		url = 'http://openfmri.s3.amazonaws.com/tarballs/ds052_raw.tgz'
 		opts = {'uncompress': True}
-		files = [('ds052_raw', url, opts)]
+		files = [('ds052', url, opts)]
 		files = self.fetcher.fetch(files, resume=resume, force=force, verbose=verbose)
+		anat_files_names = ['highres001.nii.gz','highres001_brain.nii.gz', 
+					'highres001_brain_mask.nii.gz',
+					'highres002.nii.gz', 'inplane001.nii.gz',
+					'inplane001_brain.nii.gz']
+		func_dirs = ['task001_run001','task001_run002','task002_run001','task002_run002']
+		n_subjects = 14
+		anat_files = [
+				os.path.join('ds052','sub%03d' % i, 'anatomy', anat_file)
+				for i in range(1, n_subjects+1)
+				for anat_file in anat_files_names
+				if not (i == 2 and anat_file == 'highres002.nii.gz') and not i==11
+		]
+		func_files = [
+				os.path.join('ds052','sub%03d' % i, 'BOLD', func_dir, 'bold.nii.gz')
+				for i in range(1, n_subjects+1)
+				for func_dir in func_dirs
+				if not i==11
+		]
 		# return the data
-		return Bunch(func=files)
+		return Bunch(func=func_files, anat = anat_files)
 
 
+		
+	
--- nidata/functional/my_dataset/example1.py
+++ nidata/functional/my_dataset/example1.py
@@ -1,5 +1,98 @@
+import os
+import nibabel
 import sys
+from matplotlib import pyplot as plt  # we need to call plt.show()
+from nilearn.plotting import plot_stat_map
+from nilearn.image import mean_img
+from sklearn.datasets.base import Bunch
+import os
+import pandas as pd
+import numpy as np
+from nilearn.masking import compute_epi_mask
+from nilearn.plotting import plot_roi
+from nilearn.input_data import NiftiMasker
+
 sys.path.append('E:/gitclone/nidata/')
+
 from nidata.functional.my_dataset.datasets import MyDatasets
 
-print(MyDatasets().fetch())
+data_dict = MyDatasets().fetch()
+pc_dir = 'C:\Users\oferg_000/nidata_path\my_dataset'
+func_dirs = ['task001_run001','task001_run002','task002_run001','task002_run002']
+n_subjects = 14
+num_sec = 468
+model_dict = dict()
+for func_dir in func_dirs:
+	for j in range(1,3):
+		model_files = [
+				os.path.join(pc_dir,'ds052','sub%03d' % i, 'model\model001\onsets', func_dir, 'cond%03d.txt' % j)
+				for i in range(1, n_subjects+1)			
+				if not i==11
+		]
+		model_dict[func_dir+'_cond%d' %j] =  model_files
+#print(model_dict.keys())
+n_scans = 128
+tr = 2.4
+# input paradigm information
+#frametimes = np.linspace(0, (n_scans - 1) * tr, n_scans)
+#print frametimes
+# conditions are 0 1 0 1 0 1 ...
+#conditions = np.arange(20) % 2
+#print conditions
+#model_dict = Bunch(model = model_files)
+
+
+
+for func_dir in func_dirs:
+	for model_i in model_dict[func_dir+'_cond1']:
+		cond1 = pd.read_csv(model_i, sep='\t', header=None)
+		cond2 = pd.read_csv(model_i[:-5]+'2.txt', sep='\t', header=None)
+		cond1 = cond1[0].as_matrix().tolist()
+		cond2 = cond2[0].as_matrix().tolist()
+		cnd_c1 = 0
+		cnd_c2 = 0
+		conditions = np.zeros(len(cond1)+len(cond2))
+		for counter in range(0,len(cond1)+len(cond2)-1):
+			if cond1[cnd_c1] < cond2[cnd_c2]:
+				conditions[counter] = 1
+				cnd_c1 += 1
+			else:
+				conditions[counter] = 2
+				cnd_c2 += 1
+		print conditions
+
+	#cond1 = pd.read_csv(model_dict[func_dir+'_cond1'], sep='\t', header=None)
+	#cond2 = pd.read_csv(model_dict[func_dir+'_cond2'], sep='\t', header=None)
+	
+	"""
+
+	"""
+	
+	
+	
+"""
+for func_dir in func_dirs:
+	for j in range
+for model_i in model_dict['task001_run001_cond1']:
+	vari2 = pd.read_csv(model_i, sep='\t', header=None)
+	print vari2[0].as_matrix().tolist()
+
+
+counter = 0
+for func_file in data_dict['func']:
+	counter += 1
+	if counter < 2:
+		img = nibabel.load(os.path.join(pc_dir,func_file))
+		mask_img = compute_epi_mask(img)
+		plot_roi(mask_img, mean_img(img))	
+plt.show()
+#	plot_stat_map(mean_img(img))	
+#plt.show()
+	#print(img.get_data().shape)
+#for anat_file in data_dict['anat']:
+#	img = nibabel.load(os.path.join(pc_dir,anat_file))
+#	plot_stat_map(mean_img(img))
+#plt.show()
+#print(data_dict.keys())
+"""
+
