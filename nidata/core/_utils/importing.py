fd9aec8490e0b9676678913da2f7792006b822f1
Ben Cipollini <bcipolli@ucsd.edu>
2015-11-09 11:22:07 -0800

    Fixing importing for python 2/3

--- nidata/core/_utils/importing.py
+++ nidata/core/_utils/importing.py
@@ -5,11 +5,13 @@ import os.path
 
 def import_all_submodules(dir_path, locals, globals, recursive=True):
 
-    subdirs = filter(lambda fil: os.path.isdir(os.path.join(dir_path, fil)),
+    subdirs = filter(lambda fil: os.path.isdir(os.path.join(dir_path, fil)) and
+                                 os.path.exists(os.path.join(dir_path, fil, '__init__.py')),
                      os.listdir(dir_path))
 
     for subdir in subdirs:
+        print(os.getcwd(), subdir)
         if recursive:
-            exec('from .%s import *' % subdir) in locals, globals
+            exec('from .%s import *' % subdir, locals, globals)
         else:
-            exec('from . import %s' % subdir) in locals, globals
+            exec('from . import %s' % subdir, locals, globals)
