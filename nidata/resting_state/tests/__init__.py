916e64a8bf52b5471f909ac0520bd78341f637f2
Ben Cipollini <bcipolli@ucsd.edu>
2015-11-09 12:28:03 -0800

    Try .travis.yml

--- nidata/resting_state/tests/__init__.py
+++ nidata/resting_state/tests/__init__.py
@@ -8,18 +8,18 @@ from nidata.resting_state import AbidePcpDataset, AdhdRestDataset, NyuRestDatase
 
 def test_nyu_dataset():
     # Turns out this errors; so ... expect the error until we fix it!
-    assert_raises(NotImplementedError, NyuRestDataset().fetch, n_subjects=1)
+    assert_raises(NotImplementedError, NyuRestDataset().fetch, n_subjects=1, verbose=0)
 
 
 def test_adhd_dataset():
     # Turns out this errors; so ... expect the error until we fix it!
-    assert_raises(NotImplementedError, AdhdRestDataset().fetch, n_subjects=1)
+    assert_raises(NotImplementedError, AdhdRestDataset().fetch, n_subjects=1, verbose=0)
     # dataset = AdhdRestDataset().fetch(n_subjects=1)
 
 
 def test_abide_dataset():
     # Turns out this errors; so ... expect the error until we fix it!
     # assert_raises(Exception, AbidePcpDataset().fetch, n_subjects=1)
-    dataset = AbidePcpDataset().fetch(n_subjects=1)
+    dataset = AbidePcpDataset().fetch(n_subjects=1, verbose=0)
     assert_in('phenotypic', dataset)
     assert_in('func_preproc', dataset)
