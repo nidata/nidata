aaf43329fe9d38f9fd3378d32da96ff1df50fd76
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-23 08:34:01 -0800

    BF: missing os import

--- nidata/core/_utils/importing.py
+++ nidata/core/_utils/importing.py
@@ -1,13 +1,15 @@
 """
 """
+
+import os
 import os.path as op
 
 
 def import_all_submodules(dir_path, locals, globals, recursive=True):
 
-    subdirs = filter(lambda fil: op.isdir(op.join(dir_path, fil)) and
-                                 op.exists(op.join(dir_path, fil, '__init__.py')),
-                     os.listdir(dir_path))
+    subdirs = (filter(lambda fil: op.isdir(op.join(dir_path, fil)) and
+               op.exists(op.join(dir_path, fil, '__init__.py')),
+                      os.listdir(dir_path)))
 
     for subdir in subdirs:
         if recursive:
