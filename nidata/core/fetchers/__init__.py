df322bfd7b68ac6bc626622be9fe89f079501793
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-23 21:13:55 -0800

    BF: add FetcherFunctionFetcher to fetchers.__all__

--- nidata/core/fetchers/__init__.py
+++ nidata/core/fetchers/__init__.py
@@ -1,5 +1,6 @@
 from .aws_fetcher import AmazonS3Fetcher
 from .http_fetcher import HttpFetcher
-from .base import Fetcher
+from .base import Fetcher, FetcherFunctionFetcher
 
-__all__ = ['AmazonS3Fetcher', 'HttpFetcher', 'Fetcher']
+__all__ = ['AmazonS3Fetcher', 'HttpFetcher', 'Fetcher',
+           'FetcherFunctionFetcher']
