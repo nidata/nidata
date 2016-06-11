d63252eac8a89e4844e4a60d53a2994c8d7c84a0
Ben Cipollini <bcipolli@ucsd.edu>
2016-06-10 21:15:48 -0700

    BF: avoid lambda function with 'exec'

--- nidata/core/_utils/importing.py
+++ nidata/core/_utils/importing.py
@@ -5,12 +5,19 @@ import os
 import os.path as op
 
 
+def _is_module_dir(dir_path):
+    return op.isdir(dir_path) and op.exists(op.join(dir_path, '__init__.py'))
+
+
+def _get_all_subdirs(dir_path):
+    all_paths = (op.join(dir_path, fil) for fil in os.listdir(dir_path))
+    all_modules = filter(_is_module_dir, all_paths)
+    all_module_names = (op.basename(fil) for fil in all_modules)
+    return all_module_names
+
+
 def import_all_submodules(dir_path, locals, globals, recursive=True):
-
-    subdirs = (filter(lambda fil: op.isdir(op.join(dir_path, fil)) and
-               op.exists(op.join(dir_path, fil, '__init__.py')),
-                      os.listdir(dir_path)))
-
+    subdirs = _get_all_subdirs(dir_path)
     for subdir in subdirs:
         if recursive:
             exec('from .%s import *' % subdir, locals, globals)
